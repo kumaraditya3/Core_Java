778986392d268266133c9963371a4c7725fc823c
Aditya Kumar <114847525+kumaraditya3@users.noreply.github.com>
2024-01-19 13:56:32 +0530

    Assignment Operators

---
+++ src/AssignmentOperator/AssignmentOperator.java
@@ -0,0 +1,11 @@
+package AssignmentOperator;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int p = 455 , q=13;
+        System.out.println(p+=q);
+        System.out.println(p-=q);
+        System.out.println(p/=q);
+        System.out.println(p%=q);
+    }
+}
--- src/LogicalOperators/LogicalOperators.java
+++ src/LogicalOperators/LogicalOperators.java
@@ -5,7 +5,7 @@ public class LogicalOperators {
         int a = 10, b= 15 , c =5;
         System.out.println((a>b)&&(a>c));  //false
         System.out.println((a<b)||(b<c)); //true
-        System.out.println(!(a==b)); //True
+        System.out.println(!(a==b));//True
 
     }
 }
