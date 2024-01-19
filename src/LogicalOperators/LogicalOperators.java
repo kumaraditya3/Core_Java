65661fa38f65d0550e881610c0c7d51976b2ba88
Aditya Kumar <114847525+kumaraditya3@users.noreply.github.com>
2024-01-19 13:47:06 +0530

    Logical Operators

--- src/LogicalOperators/LogicalOperators.java
+++ src/LogicalOperators/LogicalOperators.java
@@ -3,9 +3,9 @@ package LogicalOperators;
 public class LogicalOperators {
     public static void main(String[] args) {
         int a = 10, b= 15 , c =5;
-        System.out.println((a>b)&&(a>c));//false
-        System.out.println((a<b)||(b<c));//true
-        System.out.println(!(a==b));//True
+        System.out.println((a>b)&&(a>c));  //false
+        System.out.println((a<b)||(b<c)); //true
+        System.out.println(!(a==b)); //True
 
     }
 }
