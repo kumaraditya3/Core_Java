a170777d3c20146d7fc9ae10fd08f32fd3de2cc7
Aditya Kumar <114847525+kumaraditya3@users.noreply.github.com>
2024-01-19 13:16:52 +0530

    Relational Operators resulting in bool values on comparing two numbers.

---
+++ src/RelationalOperators/RelationalOperators.java
@@ -0,0 +1,16 @@
+package RelationalOperators;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a=10;
+        int b = 15;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+
+
+}
