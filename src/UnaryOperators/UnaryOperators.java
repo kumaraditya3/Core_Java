f1c709da259e2cbecef5addcb50e34ab75ad188a
Aditya Kumar <114847525+kumaraditya3@users.noreply.github.com>
2024-01-19 14:08:26 +0530

    Unary Operators

---
+++ src/UnaryOperators/UnaryOperators.java
@@ -0,0 +1,10 @@
+package UnaryOperators;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int a = +19 , b=-19, c=0;//We have used unary operator before both the values of a and b
+        System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(c!=0);
+    }
+}
